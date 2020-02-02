f63cc871c3a5ad9505ff60d5235c495aa3043097
Matthew Gleich <matthewgleich@gmail.com>
2020-02-02 14:31:27 -0500

    Update find_target.py

--- vision/find_target.py
+++ vision/find_target.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 import manipulate_image as MI
 import validate_target as VT
@@ -18,7 +19,7 @@ def find_valids(img_orig, calibration, desired_cnt):
     finds valid targets comparing to the rectangle contours, calculates the angle to target center,
     and provides graphical representations for future use.
     """
-
+    path = "/" + "/".join(os.getcwd().split("/")[1:3]) + "/test_images/"
     debug = calibration['debug']
     search = calibration['search']
     angle = 1000
@@ -29,45 +30,47 @@ def find_valids(img_orig, calibration, desired_cnt):
     if debug:
         timer_ft = SW('ft')
         timer_ft.start()
-    hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
+    #hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
     if debug:
         elapsed = timer_ft.get()
         print("DEBUG: cvt took " + str(elapsed))
     if debug:
         timer_ft.start()
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    #mask = cv2.inRange(hsv, lower_bound, upper_bound)
     if debug:
         elapsed = timer_ft.get()
         print("DEBUG: inrange took " + str(elapsed))
     if debug:
         timer_ft.start()
-    erode_and_diliate = MI.erodeAndDilate(mask)
+    #erode_and_diliate = MI.erodeAndDilate(mask)
     if debug:
         elapsed = timer_ft.get()
         print("DEBUG: erode_and_dilate took " + str(elapsed))
     if debug:
         timer_ft.start()
-    ret, mask_thresh = cv2.threshold(
-        erode_and_diliate, 127, 255, cv2.THRESH_BINARY)
+    # ret, mask_thresh = cv2.threshold(
+        # erode_and_diliate, 127, 255, cv2.THRESH_BINARY)
+        ret, mask_thresh = cv2.threshold(img_orig, 127, 255, cv2.THRESH_BINARY)
     if debug:
         elapsed = timer_ft.get()
         print("DEBUG: threshold took " + str(elapsed))
     if debug:
         time = datetime.now().strftime("%s")
-        cv2.imwrite("/home/linaro/test_images/" +
+        cv2.imwrite(path +
                     time+"image_orig.png", img_orig)
-        cv2.imwrite("/home/linaro/test_images/"+time+"mask.png", mask)
-        cv2.imwrite("/home/linaro/test_images/"+time +
-                    "erode_and_diliate.png", erode_and_diliate)
+        #cv2.imwrite(path+time+"mask.png", mask)
+        cv2.imwrite(path+time+"mask.png", mask_thresh)
+        # cv2.imwrite(path+time +
+        #             "erode_and_diliate.png", erode_and_diliate)
     if search:
         valid, cnt = VT.find_valid_target(mask_thresh, desired_cnt)
         if valid:
             valid_update = True
-            hull = cv2.convexHull(cnt)
+            hull = cv2.convexHull(cnt[0])
             cx, cy = IC.findCenter(hull)
             if debug:
                 line_img = MI.drawLine2Target(mask_thresh, cx, cy)
-                cv2.imwrite("/home/linaro/test_images/" +
+                cv2.imwrite(path +
                             time + "target_lined.png", line_img)
             angle = IC.findAngle(img_orig, cx)
     return angle, valid_update
