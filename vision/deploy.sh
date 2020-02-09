d139dbd97c2fb45a352295d0ec6f54ea870e7381
ionigman-501 <ionigman@gmail.com>
2020-02-09 12:42:05 -0500

    modify deploy to use new keys and new directory structure

--- vision/deploy.sh
+++ vision/deploy.sh
@@ -2,18 +2,24 @@
 ### deploys vision code to the raspberry pi (tinkerboard)
 ### must be run from the root of the vision repository
 
-if [ -f util/.ssh/vision_rsa.pub ]; then
+hostname="raspberrypi.local"
+home="/home/pi"
+user="pi"
+
+if [ -f util/.ssh/id_rsa.pub ]; then
    echo "Vision public key found."
    list=$(find $PWD -type f | grep -vE ".idea|.git|__pycache__")
    dirs=$(find . -type d |grep -vE ".git|.idea|__pycache__"| sed 's/^\.$//g; s/\.\///g')
    pwd=$(pwd)
    cd ..
-   base="linaro@tinkerboard.local:/home/linaro"
+   # base="linaro@tinkerboard.local:/home/linaro"
+   base=$user@$hostname:$home
+   echo $base
 
    echo "Creating remote directories..."
    for directory in $dirs;
       do
-         ssh -i $pwd/util/.ssh/vision_rsa linaro@tinkerboard.local "mkdir -p /home/linaro/$directory"
+         ssh -i $pwd/util/.ssh/id_rsa $user@$hostname "mkdir -p $home/$directory"
          if [ $? -eq 0 ]; then
             echo "Created $directory"
          else
@@ -29,7 +35,7 @@ if [ -f util/.ssh/vision_rsa.pub ]; then
          #echo $file
          next_dir=$(sed -e 's,'"$pwd"',,g' <<<"$file")
          destination=$base$next_dir
-         scp -q -r -i $pwd/util/.ssh/vision_rsa $file $destination;
+         scp -q -r -i $pwd/util/.ssh/id_rsa $file $destination;
          if [ $? -eq 0 ]; then
             code="good"
             echo "Deployed $next_dir"
